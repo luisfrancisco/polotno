a22ac4c13089a1d214fe9c3c8dee92c390143b78
Luis Francisco <lfbaroni@gmail.com>
2024-06-23 11:58:09 -0700

    install polotno

---
+++ polotnoeditor/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+const Editor = dynamic(() => import('../components/editor'), {
+  ssr: false,
+});
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Editor />
+    </div>
+  );
+}
